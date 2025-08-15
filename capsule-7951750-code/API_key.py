6a6061a79dac3480a455eb8dc6d5e2e8749f93fb
gnchang <guna9040@gmail.com>
2025-08-15 14:10:29 +0900

    Delete capsule-7951750-code directory

--- capsule-7951750-code/API_key.py
+++
@@ -1,7 +0,0 @@
-from openai import OpenAI
-
-client = OpenAI (
-    api_key= "your_key",
-    organization= "your_organization",
-    project= "your_project"
-)
--- capsule-7951750-code/CorrectBench/README.txt
+++
@@ -1,3 +0,0 @@
-You must copy the following environment to run the testbench generation.
-
-https://github.com/AutoBench/CorrectBench
